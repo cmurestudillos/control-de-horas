872b6b65ed7c906ae3d0ca713b9f8562038790a8
Carlos Mur <cmurdeveloper@gmail.com>
2024-03-12 14:48:14 +0100

    add: New Rules

--- client/src/app/api/endpoint.ts
+++ client/src/app/api/endpoint.ts
@@ -1,11 +1,6 @@
 // Development
-/* const url = {
-  api: "http://localhost:4000/api"
-}; */
-
-// Production
 const url = {
-  api: "https://controldehoras-service-dot-backend-services-api.ew.r.appspot.com/api"
-};
+  api: "http://localhost:4000/api"
+}; 
 
 export default url;
